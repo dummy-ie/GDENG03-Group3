7c71ca62d2c643e4f215f9fe8fee3f4091e15b1b
tyrtr456 <126553735+tyrtr456@users.noreply.github.com>
2024-09-19 17:42:29 +0800

    Add files via upload

---
+++ Vertex.cpp
@@ -0,0 +1,13 @@
+#include "Vertex.h"
+
+Vertex::Vertex(int x, int y, int z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+Vertex::~Vertex() {
+}
+
+
---
+++ Vertex.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Vertex
+{
+public:
+	Vertex(int x, int y, int z);
+	~Vertex();
+
+private:
+	float x;
+	float y;
+	float z;
+
+};
+
