2de2ce8481c6bb2b49f86071ed64850785788b7b
ALTelesto <joachim.arguelles@gmail.com>
2024-11-30 20:59:08 +0800

    added scroll to hierarchy

--- DirectXGame/HierarchyScreen.cpp
+++ DirectXGame/HierarchyScreen.cpp
@@ -14,11 +14,13 @@ namespace mrlol
 		ImGui::SetNextWindowSize(ImVec2(UIManager::WindowWidth / 6, UIManager::WindowHeight), ImGuiCond_Once);
 		ImGui::Begin("Scene Hierarchy");
 
+		ImGui::BeginChild("Hierarchy");
 		for (const auto& gameObject : GameObjectManager::get()->getAllObjects())
 		{
 			if (ImGui::Button(gameObject->getUniqueName().c_str(), ImVec2(250, 0)))
 				GameObjectManager::get()->setSelectedObject(gameObject.get());
 		}
+		ImGui::EndChild();
 
 		ImGui::End();
 	}
