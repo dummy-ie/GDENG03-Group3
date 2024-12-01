4f746d83704d83207f9357de3a02f6cd42f68f16
Kirvvs <jb.ursua13@gmail.com>
2024-12-01 18:58:50 +0800

    minor changes

--- DirectXGame/Matrix4x4.h
+++ DirectXGame/Matrix4x4.h
@@ -254,6 +254,21 @@ namespace gdeng03
 			return transposed;
 		}
 
+		friend std::ostream& operator<< (std::ostream& outs, const Matrix4x4& mat) {
+			return outs << "\n\n(" << mat.mat[0][0] << ", " << mat.mat[0][1] << ", " << mat.mat[0][2] << ", " << mat.mat[0][3] << ")" << "\n"
+				<< "(" << mat.mat[1][0] << ", " << mat.mat[1][1] << ", " << mat.mat[1][2] << ", " << mat.mat[1][3] << ")" << "\n"
+				<< "(" << mat.mat[2][0] << ", " << mat.mat[2][1] << ", " << mat.mat[2][2] << ", " << mat.mat[2][3] << ")" << "\n"
+				<< "(" << mat.mat[3][0] << ", " << mat.mat[3][1] << ", " << mat.mat[3][2] << ", " << mat.mat[3][3] << ")" << "\n";
+		}
+
+		std::string toString() const
+		{
+			return  "\n\n(" + std::to_string(mat[0][0]) + ", " + std::to_string(mat[0][1]) + ", " + std::to_string(mat[0][2]) + ", " + std::to_string(mat[0][3]) + ")" + "\n"
+				+ "(" + std::to_string(mat[1][0]) + ", " + std::to_string(mat[1][1]) + ", " + std::to_string(mat[1][2]) + ", " + std::to_string(mat[1][3]) + ")" + "\n"
+				+ "(" + std::to_string(mat[2][0]) + ", " + std::to_string(mat[2][1]) + ", " + std::to_string(mat[2][2]) + ", " + std::to_string(mat[2][3]) + ")" + "\n"
+				+ "(" + std::to_string(mat[3][0]) + ", " + std::to_string(mat[3][1]) + ", " + std::to_string(mat[3][2]) + ", " + std::to_string(mat[3][3]) + ")" + "\n";
+		}
+
 		float* getMatrix()
 		{
 			return *mat;
